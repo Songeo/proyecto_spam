a9e8748495c54dc45a354af1ecba9127233966b2
Songeo <sonia.gmcl@gmail.com>
2017-05-26 14:27:32 -0500

    matriz de variables binarias para probar

--- src/02_train_test_samples.R
+++ src/02_train_test_samples.R
@@ -6,9 +6,33 @@ load("cache/df.spam.RData")
 load("cache/tab.spam.rsparse.RData")
 
 
+
+
 # ............................................................................ # 
 
-# Document-Term Matrix
+# Document-Term Matrix Bin
+tab.spam.rsparse %>% head
+
+dtm.df <- tab.spam.rsparse %>%
+  # filter(tf_idf > 0) %>%
+  mutate(bin = 1) %>% 
+  dplyr::select(document, term, bin) %>% 
+  spread(term, bin, fill = 0) %>% 
+  left_join(
+    tab.spam.rsparse %>% 
+      group_by(document, spam) %>% 
+      dplyr::summarise(n_words = sum(count)), 
+    by = "document"
+  ) #%>% 
+  # dplyr::select(document, spam, n_words, able:years)
+dtm.df  %>% data.frame() %>% head
+
+
+
+
+# ............................................................................ # 
+
+# Document-Term Matrix tf_idf
 tab.spam.rsparse %>% head
 
 dtm.df <- tab.spam.rsparse %>%
@@ -20,8 +44,8 @@ dtm.df <- tab.spam.rsparse %>%
       group_by(document, spam) %>% 
       dplyr::summarise(n_words = sum(count)), 
     by = "document"
-  ) %>% 
-  dplyr::select(document, spam, n_words, able:years)
+  ) #%>% 
+# dplyr::select(document, spam, n_words, able:years)
 dtm.df  %>% data.frame() %>% head
 
 
@@ -31,26 +55,26 @@ dtm.df  %>% data.frame() %>% head
 nrow(dtm.df)
 
 indice1 <- which(dtm.df$spam==1)
-length(indice1)
-
 indice0 <- which(dtm.df$spam==0)
+
+length(indice1)
 length(indice0)
+100*(1-length(indice1)/(length(indice1) + length(indice0)))
 
 
 set.seed(105720)
 # cerca del 80% es una muestra de entrenamiento
-indice1train <- sample(indice1, 1200, replace=F) 
-indice0train <- sample(indice0, 1200, replace=F) 
+indice1train <- sample(indice1, 800, replace=F) 
+indice0train <- sample(indice0, 800, replace=F) 
 indicetrain <- c(indice1train,indice0train)
-
 
 y.train <- dtm.df$spam[indicetrain]
 y.test <- dtm.df$spam[-indicetrain]
 
 x.train <- dtm.df[indicetrain, ] %>% 
-  dplyr::select(-(document:spam))
+  dplyr::select(-document, -spam)
 x.test <- dtm.df[-indicetrain, ] %>% 
-  dplyr::select(-(document:spam))
+  dplyr::select(-document, -spam)
 
 dim(x.train) + dim(x.test)
 nrow(dtm.df)
