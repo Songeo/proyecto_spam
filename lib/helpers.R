c5cad16beaa8a8518053c5794b6c9906e927a79a
Songeo <sonia.gmcl@gmail.com>
2017-05-30 00:40:36 -0500

    Funcion de Curva Roc

--- lib/helpers.R
+++ lib/helpers.R
@@ -1,2 +1,18 @@
 library(ggplot2)
 theme_set(theme_light())
+
+
+simple_roc <- function(labels, scores){
+  labels <- labels[order(scores, decreasing=TRUE)]
+  data.frame(TPR=cumsum(labels)/sum(labels), FPR=cumsum(!labels)/sum(!labels), labels)
+}
+
+CRoc_GG <- function(obs, pred, mod.tit){
+  simple_roc(obs, 
+             pred) %>% 
+    ggplot(aes(x = FPR, y = TPR)) +
+    geom_abline(slope = 1, intercept = 0, 
+                color = "salmon", linetype = 2) +
+    geom_line(color = "gray60", size= 1) + 
+    ggtitle(paste("Curva ROC", mod.tit))
+}
