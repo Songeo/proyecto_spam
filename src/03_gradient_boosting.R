7e3e57c5eb12fe404302e8ca861776aa9287663a
Songeo <sonia.gmcl@gmail.com>
2017-05-30 01:05:20 -0500

    gbm std out

--- src/03_gradient_boosting.R
+++ src/03_gradient_boosting.R
@@ -1,8 +1,9 @@
 
 library(ProjectTemplate)
-load.project()
+reload.project()
 
-library(randomForest)
+load("cache/dtm_train.RData")
+
 library(gbm)
 
 
@@ -10,41 +11,73 @@ library(gbm)
 
 # df train test
 df.train <- dtm.df[indicetrain, ] %>% 
-  dplyr::select(-document) %>% #, -n_words) 
-  mutate(spam = factor(spam))
+  dplyr::select(-document) #%>% #, -n_words) 
+  # mutate(spam = factor(spam))
 
 df.test <- dtm.df[-indicetrain, ] %>% 
-  dplyr::select(-document) %>% #, -n_words) 
-  mutate(spam = factor(spam))
+  dplyr::select(-document) #%>% #, -n_words) 
+  # mutate(spam = factor(spam))
 
 # ............................................................................ # 
 
-num.trees <- 3000
-
 # boosted tree
-gbm(formula = Response ~ Var1 + Var2 + Cat5 + Cat6,
-    distribution = "bernoulli",
-    data = train,
-    n.trees = 2500,
-    shrinkage = .01,
-    n.minobsinnode = 20)
+trees.num <- 4000
 
-
-
+set.seed(105720)
 boost.mod <- gbm(spam ~ ., 
                  data = df.train,
+                 # cv.folds = 3,
                  distribution='bernoulli',
-                 n.trees=B,
-                 shrinkage=0.01,
-                 train.fraction=1.)   
+                 n.trees = trees.num,
+                 shrinkage = 0.01,
+                 train.fraction = 1.)   
+gbm.perf(boost.mod)
 
 
-table(predict(boost.mod, type = "response"), 
-      df.train$spam)
 
 # entrenamiento
-tab <- table(predict(boost.mod, newdata = df.test), 
-             df.test$spam) 
-tab
-prop.table(tab, 1) 
-prop.table(tab, 2) 
+preds.train <- predict.gbm(boost.mod, 
+                           newdata = df.train, 
+                           n.trees = trees.num, 
+                           type="response") %>% 
+  round
+preds.train %>% head
+
+(tab.acc.train <- table(df.train$spam ==  preds.train))
+(tab.conf.train <- table(df.train$spam, preds.train))
+
+# roc train
+ggroc.train <- CRoc_GG(obs = df.train$spam,
+                       pred = preds.train,
+                       mod.tit = "Gradient Boosting (Entrenamiento)")
+ggroc.train
+
+
+# ............................................................................ # 
+
+# Test results
+preds.test <- predict.gbm(boost.mod, 
+                          newdata = df.test, 
+                          n.trees = trees.num, 
+                          type="response") %>% 
+  round
+preds.test %>% head
+
+(tab.acc.test <- table(df.test$spam ==  preds.test))
+(tab.conf.test <- table(df.test$spam, preds.test))
+
+ggroc.test <- CRoc_GG(obs  = df.test$spam,
+                      pred = preds.test,
+                      mod.tit = "Gradient Boosting (Prueba)")
+ggroc.test
+
+
+
+# ............................................................................ # 
+
+# Saving parameters
+
+results.gb <- list(tab.acc.train, tab.conf.train, ggroc.train,
+                   tab.acc.test, tab.conf.test, ggroc.test)
+
+save(results.gb, file = "cache/results_models/results_gboost.Rdata")
