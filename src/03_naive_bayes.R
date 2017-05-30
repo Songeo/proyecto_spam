63f943b7294938855022f37ea78c11356145f4e9
Songeo <sonia.gmcl@gmail.com>
2017-05-30 01:05:12 -0500

    nb std out

--- src/03_naive_bayes.R
+++ src/03_naive_bayes.R
@@ -25,20 +25,38 @@ nb.mod <- naiveBayes(formula = spam~., data = df.train)
 preds.train <- predict(object = nb.mod, newdata = df.train)
 preds.train %>% head
 
-table(df.train$spam, preds.train)
-table(df.train$spam, preds.train) %>% prop.table(2)
-verification::roc.plot(x = df.train$spam,
-                       pred = preds.train,
-                       plot = 'both', binormal =T, CI = T, n.boot = 500)
+
+(tab.acc.train <- table(df.train$spam ==  preds.train))
+(tab.conf.train <- table(df.train$spam, preds.train))
+
+# roc train
+ggroc.train <- CRoc_GG(obs = as.numeric(as.character(df.train$spam)),
+        pred = as.numeric(as.character(preds.train)),
+        mod.tit = "Bayes Ingenuo (Entrenamiento)")
+ggroc.train
 
 
+# ............................................................................ # 
 
 # Test results
 preds.test <- predict(object = nb.mod, newdata = df.test)
 preds.test %>% head
 
-table(df.test$spam, preds.test)
-table(df.test$spam, preds.test) %>% prop.table(2)
+(tab.acc.test <- table(df.test$spam ==  preds.test))
+(tab.conf.test <- table(df.test$spam, preds.test))
 
-verification::roc.plot(x = df.test$spam,
-                       pred = preds.test)
+ggroc.test <- CRoc_GG(obs = as.numeric(as.character(df.test$spam)),
+        pred = as.numeric(as.character(preds.test)),
+        mod.tit = "Bayes Ingenuo (Prueba)")
+ggroc.test
+
+
+
+# ............................................................................ # 
+
+# Saving parameters
+
+results.nb <- list(tab.acc.train, tab.conf.train, ggroc.train,
+     tab.acc.test, tab.conf.test, ggroc.test)
+
+save(results.nb, file = "cache/results_models/results_nb.Rdata")
